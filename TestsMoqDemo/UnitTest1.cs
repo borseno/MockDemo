725d00a445fba5493f83bdc589aaec627ac9de20
borseno <borseno2@gmail.com>
2019-09-29 20:20:05 +0300

    AddedExceptionTest

--- TestsMoqDemo/UnitTest1.cs
+++ TestsMoqDemo/UnitTest1.cs
@@ -4,10 +4,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 using Xunit;
 
 namespace TestsMoqDemo
 {
+    public class MyIntStringClass
+    {
+        public string Value { get; set; }
+        public int Boolean { get; set; }
+    }
+
     public class UnitTest1
     {
         [Fact]
@@ -32,6 +39,21 @@ namespace TestsMoqDemo
                     Assert.Equal(expected[i].Age, actual[i].Age);
                     Assert.Equal(expected[i].Name, actual[i].Name);
                 }
+            }
+        }
+
+        [Fact]
+        public async Task LoadDataAsync_ThrowsException_IfClassPropertiesDontCorrespondToRow()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                mock.Mock<ITxtFileReader>()
+                    .Setup(reader => reader.ReadAllLinesAsync())
+                    .Returns(GetSampleRowsAsync());
+
+                var accessor = mock.Create<TxtFileDataAccess>();
+
+                await Assert.ThrowsAsync<InvalidOperationException>(() => accessor.LoadDataAsync<MyIntStringClass>());
             }
         }
 
