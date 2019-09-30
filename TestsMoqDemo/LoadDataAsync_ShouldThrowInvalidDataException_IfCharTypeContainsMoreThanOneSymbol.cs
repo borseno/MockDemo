7deb3a1b759765c848b21e7037be3a4c160ccdc2
borseno <borseno2@gmail.com>
2019-09-30 22:08:13 +0300

    Added TestShouldThrowInvalidDataExceptionForCharMoreThan1Symbol

---
+++ TestsMoqDemo/LoadDataAsync_ShouldThrowInvalidDataException_IfCharTypeContainsMoreThanOneSymbol.cs
@@ -0,0 +1,27 @@
+﻿using Autofac.Extras.Moq;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using TxtFileDataAccess;
+using Xunit;
+
+namespace TestsMoqDemo
+{
+    public partial class LoadDataAsyncTests
+    {
+        [Fact]
+        public async Task LoadDataAsync_ShouldThrowInvalidDataException_IfCharTypeContainsMoreThanOneSymbol()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                mock.Mock<ITxtFileReader>()
+                    .Setup(reader => reader.ReadAllLinesAsync())
+                    .Returns(Task.FromResult((IEnumerable<string>)new[] { "Symbol=123data!;Data=s;" }));
+
+                var accessor = mock.Create<TxtFileDataAccess<SampleClassWithTwoChars>>();
+
+                await Assert.ThrowsAsync<InvalidDataException>(() => accessor.LoadDataAsync());
+            }
+        }
+    }
+}
